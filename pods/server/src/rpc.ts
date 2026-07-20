intabia-fusion/platform

Fix guest identity to match other social ids
Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

pods/server/src/rpc.ts
@@ -31,7 +31,9 @@ import core, {
   type DocumentUpdate,
   type Mixin,
   type MixinData,
-  type MixinUpdate
+  type MixinUpdate,
+  type SocialIdType,
+  AccountRole
 } from '@hcengineering/core'
 import { rpcJSONReplacer, type RateLimitInfo } from '@hcengineering/rpc'
 import {
@@ -616,9 +618,25 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
     })
   })
 
+  interface EnsurePersonOptions {
+    addGuestEmployee?: boolean
+  }
+
   app.post('/api/v1/ensure-person/:workspaceId', (req, res) => {
     void withSession(req, res, 'ensurePerson', async (ctx, session, rateLimit, token) => {
-      const { socialType, socialValue, firstName, lastName } = (await retrieveJson(req)) ?? {}
+      const {
+        socialType,
+        socialValue,
+        firstName,
+        lastName,
+        options
+      }: {
+        socialType: SocialIdType
+        socialValue: string
+        firstName: string
+        lastName: string
+        options?: EnsurePersonOptions
+      } = (await retrieveJson(req)) ?? {}
       const accountClient = getAccountClient(token)
 
       const { uuid, socialId } = await accountClient.ensurePerson(socialType, socialValue, firstName, lastName)
@@ -626,7 +644,7 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
         session.getUser() === systemAccountUuid ? core.account.System : pickPrimarySocialId(session.getSocialIds())._id
       const txFactory: TxFactory = new TxFactory(primaryPersonId)
 
-      const [person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
+      let [person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
       let personRef: Ref<Person> = person?._id
 
       if (personRef === undefined) {
@@ -651,6 +669,20 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
 
         await session.txRaw(ctx, createUniquePersonTx)
         personRef = createPersonTx.objectId
+        if (options?.addGuestEmployee === true) {
+          ;[person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
+        }
+      }
+
+      if (person !== undefined && options?.addGuestEmployee === true) {
+        const h = ctx.pipeline.context.hierarchy
+        if (!h.hasMixin(person, contact.mixin.Employee)) {
+          const op = txFactory.createTxMixin(person._id, contact.class.Person, person.space, contact.mixin.Employee, {
+            active: true,
+            role: AccountRole.Guest
+          })
+          await session.txRaw(ctx, op)
+        }
       }
 
       const [socialIdentity] = await session.findAllRaw(

server/account/src/collections/postgres/migrations.ts
@@ -81,7 +81,8 @@ export function getMigrations (ns: string, flavor: DBFlavor): [string, string][]
     getV21Migration(ns, flavor),
     getV22Migration(ns, flavor),
     getV23Migration(ns, flavor),
-    getV24Migration(ns, flavor)
+    getV24Migration(ns, flavor),
+    getV25Migration(ns, flavor)
   ]
 }
 
@@ -781,4 +782,28 @@ function getV24Migration (ns: string, flavor: DBFlavor): [string, string] {
     ON ${ns}.workspace_permissions (permission);
     `
   ]
+}
+
+function getV25Migration (ns: string, flavor: DBFlavor): [string, string] {
+  // For PostgreSQL, we need to check if the value exists before adding it
+  const addValueSql =
+    flavor === 'postgres'
+      ? `
+    -- Add office value to social_id_type enum (PostgreSQL)
+    DO $$     BEGIN
+        IF NOT EXISTS (
+            SELECT 1 FROM pg_enum
+            WHERE enumlabel = 'office'
+            AND enumtypid = (SELECT oid FROM pg_type WHERE typname = 'social_id_type' AND typnamespace = (SELECT oid FROM pg_namespace WHERE nspname = '${ns}'))
+        ) THEN
+            ALTER TYPE ${ns}.social_id_type ADD VALUE 'office';
+        END IF;
+    END $$;
+    `
+      : `
+    -- Add office value to social_id_type enum (CockroachDB)
+    ALTER TYPE ${ns}.social_id_type ADD VALUE IF NOT EXISTS 'office';
+    `
+
+  return ['account_db_v25_add_office_social_id_type', addValueSql]
 }

TraceX-dev/TraceX

(cherry picked from commit 662d376d325798a75dc9e1820945f764767389f3)

pods/server/src/rpc.ts
@@ -31,7 +31,9 @@ import core, {
   type Space,
   type Timestamp,
   type TxCUD,
-  type TxDomainEvent
+  type TxDomainEvent,
+  type SocialIdType,
+  AccountRole
 } from '@hcengineering/core'
 import { rpcJSONReplacer, type RateLimitInfo } from '@hcengineering/rpc'
 import { wrapPipeline, type ClientSessionCtx, type ConnectionSocket, type Session, type SessionManager } from '@hcengineering/server-core'
@@ -621,9 +623,25 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
     })
   })
 
+  interface EnsurePersonOptions {
+    addGuestEmployee?: boolean
+  }
+
   app.post('/api/v1/ensure-person/:workspaceId', (req, res) => {
     void withSession(req, res, 'ensurePerson', async (ctx, session, rateLimit, token) => {
-      const { socialType, socialValue, firstName, lastName } = (await retrieveJson(req)) ?? {}
+      const {
+        socialType,
+        socialValue,
+        firstName,
+        lastName,
+        options
+      }: {
+        socialType: SocialIdType
+        socialValue: string
+        firstName: string
+        lastName: string
+        options?: EnsurePersonOptions
+      } = (await retrieveJson(req)) ?? {}
       const accountClient = getAccountClient(token)
 
       const { uuid, socialId } = await accountClient.ensurePerson(socialType, socialValue, firstName, lastName)
@@ -631,7 +649,7 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
         session.getUser() === systemAccountUuid ? core.account.System : pickPrimarySocialId(session.getSocialIds())._id
       const txFactory: TxFactory = new TxFactory(primaryPersonId)
 
-      const [person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
+      let [person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
       let personRef: Ref<Person> = person?._id
 
       if (personRef === undefined) {
@@ -656,6 +674,20 @@ export function registerRPC (app: Express, sessions: SessionManager, ctx: Measur
 
         await session.txRaw(ctx, createUniquePersonTx)
         personRef = createPersonTx.objectId
+        if (options?.addGuestEmployee === true) {
+          ;[person] = await session.findAllRaw(ctx, contact.class.Person, { personUuid: uuid }, { limit: 1 })
+        }
+      }
+
+      if (person !== undefined && options?.addGuestEmployee === true) {
+        const h = ctx.pipeline.context.hierarchy
+        if (!h.hasMixin(person, contact.mixin.Employee)) {
+          const op = txFactory.createTxMixin(person._id, contact.class.Person, person.space, contact.mixin.Employee, {
+            active: true,
+            role: AccountRole.Guest
+          })
+          await session.txRaw(ctx, op)
+        }
       }
 
       const [socialIdentity] = await session.findAllRaw(

server/account/src/collections/postgres/migrations.ts
@@ -83,7 +83,8 @@ export function getMigrations (ns: string, flavor: DBFlavor): [string, string][]
     getV23Migration(ns, flavor),
     getV24Migration(ns, flavor),
     getV25Migration(ns, flavor),
-    getV26Migration(ns, flavor)
+    getV26Migration(ns, flavor),
+    getV27Migration(ns, flavor)
   ]
 }
 
@@ -808,4 +809,28 @@ function getV26Migration (ns: string, flavor: DBFlavor): [string, string] {
     ADD COLUMN IF NOT EXISTS pending_configuration JSONB;
     `
   ]
+}
+
+function getV27Migration (ns: string, flavor: DBFlavor): [string, string] {
+  // For PostgreSQL, we need to check if the value exists before adding it
+  const addValueSql =
+    flavor === 'postgres'
+      ? `
+    -- Add office value to social_id_type enum (PostgreSQL)
+    DO $$     BEGIN
+        IF NOT EXISTS (
+            SELECT 1 FROM pg_enum
+            WHERE enumlabel = 'office'
+            AND enumtypid = (SELECT oid FROM pg_type WHERE typname = 'social_id_type' AND typnamespace = (SELECT oid FROM pg_namespace WHERE nspname = '${ns}'))
+        ) THEN
+            ALTER TYPE ${ns}.social_id_type ADD VALUE 'office';
+        END IF;
+    END $$;
+    `
+      : `
+    -- Add office value to social_id_type enum (CockroachDB)
+    ALTER TYPE ${ns}.social_id_type ADD VALUE IF NOT EXISTS 'office';
+    `
+
+  return ['account_db_v27_add_office_social_id_type', addValueSql]
 }