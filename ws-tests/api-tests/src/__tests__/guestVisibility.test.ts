91d811b34827e90f8b9504bf27de035220a987d2
Artem Savchenko <armisav@gmail.com>
2026-08-01 08:52:39 +0700

    Fix copyright
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- ws-tests/api-tests/src/__tests__/guestVisibility.test.ts
+++ ws-tests/api-tests/src/__tests__/guestVisibility.test.ts
@@ -1,5 +1,5 @@
 //
-// Copyright © 2026 TraceX.
+// Copyright © 2026 TraceX SAS.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
