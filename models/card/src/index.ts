10368faec0f72ac620e25635bd8401471d864284
Denis Bykhov <bykhov.denis@gmail.com>
2026-09-09 13:03:48 +0500

    feat: add reorderable and readonly props to DocTable in RelationEditor
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- models/card/src/index.ts
+++ models/card/src/index.ts
@@ -142,12 +142,14 @@ export class TCard extends TDoc implements Card {
   @Prop(TypeRef(card.class.Card), card.string.Parent)
     parent?: Ref<Card> | null
 
+  @Hidden()
   @Prop(Collection(attachment.class.Attachment), attachment.string.Attachments, { shortLabel: attachment.string.Files })
     attachments?: number
 
   @Prop(TypeRank(), core.string.Rank)
     rank!: Rank
 
+  @Hidden()
   @Prop(Collection(time.class.ToDo), getEmbeddedLabel('Action Items'))
     todos?: CollectionSize<ToDo>
 
@@ -170,6 +172,7 @@ export class TCard extends TDoc implements Card {
   @ReadOnly()
     peerId?: string
 
+  @Hidden()
   @Prop(Collection(chunter.class.ChatMessage), chunter.string.Comments)
     comments?: number
 }
