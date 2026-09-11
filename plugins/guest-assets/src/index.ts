1920c8e6405ed005fba1ba9458b4c8508f303788
Alexander Onnikov <Alexander.Onnikov@xored.com>
2026-09-11 12:38:10 +0700

    chore: refactor lang usage to make webpack happy
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- plugins/guest-assets/src/index.ts
+++ plugins/guest-assets/src/index.ts
@@ -1,5 +1,6 @@
 //
 // Copyright © 2024 Hardcore Engineering Inc.
+// Copyright © 2026 TraceX SAS.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
@@ -14,10 +15,11 @@
 //
 
 import { addStringsLoader, loadMetadata } from '@hcengineering/platform'
+import { loadLang } from './lang'
 import guest, { guestId } from '@hcengineering/guest'
 
 const icons = require('../assets/icons.svg') as string // eslint-disable-line
 loadMetadata(guest.icon, {
   Link: `${icons}#link`
 })
-addStringsLoader(guestId, async (lang: string) => await import(`../lang/${lang}.json`))
+addStringsLoader(guestId, loadLang)
