02b049740e55154c4f20c61b36bf9410d5d46f14
Alexander Onnikov <Alexander.Onnikov@xored.com>
2026-09-16 00:57:31 +0700

    fix: ui tests fixes
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- tests/sanity/tests/model/planning/planning-page.ts
+++ tests/sanity/tests/model/planning/planning-page.ts
@@ -130,10 +130,7 @@ export class PlanningPage extends CalendarPage {
       const boundingBox = await this.selectTimeCell(time, column).boundingBox()
       expect(boundingBox).toBeTruthy()
       if (boundingBox != null) {
-        await this.page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + 10)
-        await this.page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + (addHalf ? 40 : 20), {
-          steps: 5
-        })
+        await this.page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + (addHalf ? 40 : 20))
         await this.page.mouse.up()
         await expect(this.eventInSchedule(title)).toBeVisible({ timeout: 5000 })
       }
