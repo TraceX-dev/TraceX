2685f741ccc9211dcebe40155b551f25555e447b
Artem Savchenko <armisav@gmail.com>
2026-09-17 21:51:21 +0700

    Clean up
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- plugins/attachment-resources/src/components/Attachments.svelte
+++ plugins/attachment-resources/src/components/Attachments.svelte
@@ -1,7 +1,6 @@
 <!--
 // Copyright © 2020, 2021 Anticrm Platform Contributors.
 // Copyright © 2021 Hardcore Engineering Inc.
-// Copyright © 2026 TraceX SAS.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
@@ -18,7 +17,7 @@
   import { Attachment } from '@hcengineering/attachment'
   import { Class, Data, Doc, DocumentQuery, Ref, Space } from '@hcengineering/core'
   import { IntlString } from '@hcengineering/platform'
-  import { Label, resizeObserver, Scroller, Section, Spinner, ButtonIcon, IconAdd } from '@hcengineering/ui'
+  import { Icon, Label, resizeObserver, Scroller, Spinner, ButtonIcon, IconAdd } from '@hcengineering/ui'
   import view, { BuildModelKey } from '@hcengineering/view'
   import { Table } from '@hcengineering/view-resources'
   import { getClient } from '@hcengineering/presentation'
@@ -98,80 +97,67 @@
   }
 </script>
 
-<Section {label} icon={IconAttachments} {showHeader}>
-  <div slot="header" class="buttons-group small-gap">
-    {#if loading}
-      <Spinner />
-    {:else if !readonly}
-      <ButtonIcon icon={IconAdd} kind={'tertiary'} size={'small'} on:click={openFile} />
-    {/if}
-  </div>
+<div class="antiSection" use:resizeObserver={(element) => (wSection = element.clientWidth)}>
+  {#if showHeader}
+    <div class="antiSection-header">
+      <div class="antiSection-header__icon">
+        <Icon icon={IconAttachments} size={'small'} />
+      </div>
+      <span class="antiSection-header__title">
+        <Label {label} />
+      </span>
+      <div class="buttons-group small-gap">
+        {#if loading}
+          <Spinner />
+        {:else if !readonly}
+          <ButtonIcon icon={IconAdd} kind={'tertiary'} size={'small'} on:click={openFile} />
+        {/if}
+      </div>
+    </div>
+  {/if}
 
-  <div slot="content" class="flex-col" use:resizeObserver={(element) => (wSection = element.clientWidth)}>
-    <input
-      bind:this={inputFile}
-      disabled={inputFile == null}
-      multiple
-      type="file"
-      name="file"
-      id="file"
-      style="display: none"
-      on:change={fileSelected}
-    />
-    {#if !loading && (attachments === null || attachments === 0) && !readonly}
-      <AttachmentDroppable
-        bind:loading
-        bind:dragover
-        objectClass={_class}
-        {objectId}
-        {space}
-        {attachmentClass}
-        {attachmentClassOptions}
-      >
-        <div class="antiSection-empty attachments flex-col" class:mt-3={showHeader} class:solid={dragover}>
-          <div class="flex-center caption-color">
-            <UploadDuo size={'large'} />
-          </div>
-          <div class="text-sm content-dark-color" style:pointer-events="none">
-            <Label label={attachment.string.NoAttachments} />
-          </div>
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <div
-            class="over-underline text-sm caption-color"
-            style:pointer-events={dragover ? 'none' : 'all'}
-            on:click={() => {
-              inputFile.click()
-            }}
-          >
-            <Label label={attachment.string.UploadDropFilesHere} />
-          </div>
+  <input
+    bind:this={inputFile}
+    disabled={inputFile == null}
+    multiple
+    type="file"
+    name="file"
+    id="file"
+    style="display: none"
+    on:change={fileSelected}
+  />
+  {#if !loading && (attachments === null || attachments === 0) && !readonly}
+    <AttachmentDroppable
+      bind:loading
+      bind:dragover
+      objectClass={_class}
+      {objectId}
+      {space}
+      {attachmentClass}
+      {attachmentClassOptions}
+    >
+      <div class="antiSection-empty attachments flex-col" class:mt-3={showHeader} class:solid={dragover}>
+        <div class="flex-center caption-color">
+          <UploadDuo size={'large'} />
         </div>
-      </AttachmentDroppable>
-    {:else if wSection < 640}
-      <Scroller horizontal noFade={false}>
-        <Table
-          _class={attachmentClass}
-          config={[
-            '',
-            'description',
-            {
-              key: 'pinned',
-              presenter: view.component.BooleanTruePresenter,
-              label: attachment.string.Pinned,
-              sortingKey: 'pinned'
-            },
-            ...extraConfig,
-            'lastModified'
-          ]}
-          options={{ sort: { pinned: -1 }, showArchived: true }}
-          query={{ ...query, attachedTo: objectId }}
-          loadingProps={{ length: attachments ?? 0 }}
-          on:content={updateContent}
-          {readonly}
-        />
-      </Scroller>
-    {:else}
+        <div class="text-sm content-dark-color" style:pointer-events="none">
+          <Label label={attachment.string.NoAttachments} />
+        </div>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="over-underline text-sm caption-color"
+          style:pointer-events={dragover ? 'none' : 'all'}
+          on:click={() => {
+            inputFile.click()
+          }}
+        >
+          <Label label={attachment.string.UploadDropFilesHere} />
+        </div>
+      </div>
+    </AttachmentDroppable>
+  {:else if wSection < 640}
+    <Scroller horizontal noFade={false}>
       <Table
         _class={attachmentClass}
         config={[
@@ -192,6 +178,27 @@
         on:content={updateContent}
         {readonly}
       />
-    {/if}
-  </div>
-</Section>
+    </Scroller>
+  {:else}
+    <Table
+      _class={attachmentClass}
+      config={[
+        '',
+        'description',
+        {
+          key: 'pinned',
+          presenter: view.component.BooleanTruePresenter,
+          label: attachment.string.Pinned,
+          sortingKey: 'pinned'
+        },
+        ...extraConfig,
+        'lastModified'
+      ]}
+      options={{ sort: { pinned: -1 }, showArchived: true }}
+      query={{ ...query, attachedTo: objectId }}
+      loadingProps={{ length: attachments ?? 0 }}
+      on:content={updateContent}
+      {readonly}
+    />
+  {/if}
+</div>
