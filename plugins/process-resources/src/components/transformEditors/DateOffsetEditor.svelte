3c39068b669df372db38cbe46e2521cc6a53e375
Denis Bykhov <bykhov.denis@gmail.com>
2026-09-20 18:23:52 +0500

    feat: enhance date offset functionality and add tests
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/process-resources/src/components/transformEditors/DateOffsetEditor.svelte
+++ plugins/process-resources/src/components/transformEditors/DateOffsetEditor.svelte
@@ -1,5 +1,6 @@
 <!--
 // Copyright © 2025 Hardcore Engineering Inc.
+// Copyright © 2026 TraceX SAS.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
@@ -13,14 +14,49 @@
 // limitations under the License.
 -->
 <script lang="ts">
-  import { Card } from '@hcengineering/presentation'
+  import { Analytics } from '@hcengineering/analytics'
+  import type { MasterTag, Tag } from '@hcengineering/card'
+  import core from '@hcengineering/core'
+  import type { AnyAttribute, Ref } from '@hcengineering/core'
+  import { getResource } from '@hcengineering/platform'
+  import { Card, getClient } from '@hcengineering/presentation'
+  import type { Process } from '@hcengineering/process'
+  import ui, { DropdownLabelsIntl } from '@hcengineering/ui'
+  import type { AnySvelteComponent, DropdownIntlItem } from '@hcengineering/ui'
+  import view from '@hcengineering/view'
   import { createEventDispatcher } from 'svelte'
-  import process from '../../plugin'
-  import ui, { DropdownIntlItem, DropdownLabelsIntl, NumberInput } from '@hcengineering/ui'
+  import processPlugin from '../../plugin'
+  import { getContext } from '../../utils'
+  import ProcessAttribute from '../ProcessAttribute.svelte'
 
-  export let props: Record<string, any>
+  export let process: Process
+  export let masterTag: Ref<MasterTag | Tag>
+  export let attribute: AnyAttribute
+  export let props: { offset?: number | string, offsetType?: string, direction?: string } = {}
 
-  let offset = props?.offset
+  const client = getClient()
+  $: offsetAttribute = {
+    ...attribute,
+    label: processPlugin.string.Offset,
+    type: { _class: core.class.TypeNumber, label: core.string.Number }
+  }
+  $: offsetContext = getContext(client, process, core.class.TypeNumber, 'attribute', undefined, true)
+
+  let offset = props?.offset ?? 0
+  let editor: AnySvelteComponent | undefined
+
+  async function loadEditor (): Promise<void> {
+    try {
+      const hierarchy = client.getHierarchy()
+      const inlineEditor = hierarchy.as(hierarchy.getClass(core.class.TypeNumber), view.mixin.AttributeEditor).inlineEditor
+      editor = await getResource(inlineEditor)
+    } catch (error) {
+      Analytics.handleError(error instanceof Error ? error : new Error(String(error)))
+    }
+  }
+
+  void loadEditor()
+
   let offsetType = props?.offsetType ?? 'days'
   let direction = props?.direction ?? 'after'
 
@@ -44,18 +80,25 @@
       direction
     })
   }
-
-  $: params = {
-    days: offset,
-    weeks: offset,
-    months: offset
-  }
 </script>
 
-<Card on:close width={'small'} label={process.string.Offset} canSave okAction={save}>
-  <div class="flex-row-center flex-gap-2">
-    <DropdownLabelsIntl items={directions} bind:selected={direction} />
-    <NumberInput bind:value={offset} autoFocus minValue={0} focusable maxDigitsAfterPoint={0} />
-    <DropdownLabelsIntl {items} bind:selected={offsetType} {params} />
+<Card on:close width={'small'} label={processPlugin.string.Offset} canSave okAction={save}>
+  <div class="flex-col flex-gap-2">
+    <div class="flex-row-center flex-gap-2">
+      <DropdownLabelsIntl items={directions} bind:selected={direction} />
+      <DropdownLabelsIntl {items} bind:selected={offsetType} />
+    </div>
+    <ProcessAttribute
+      {process}
+      {masterTag}
+      context={offsetContext}
+      attribute={offsetAttribute}
+      presenterClass={{ attrClass: core.class.TypeNumber, category: 'attribute' }}
+      value={offset}
+      {editor}
+      on:change={(event) => {
+        offset = event.detail
+      }}
+    />
   </div>
 </Card>
